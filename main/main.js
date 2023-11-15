d10dabfafff05027b8ab4a9086bc3fbd749bba09
scromny <norboyevsardor999@gmail.com>
2023-11-15 17:54:15 +0300

    html & JavaScript

---
+++ main/main.js
@@ -0,0 +1,9 @@
+var elHeading = document.querySelector(".heading")
+var Kevin = prompt("Mablag' kiriting")
+var summa = 8317488.6;
+
+if (summa < Kevin) {
+    elHeading.textContent = ("Oq yo'l, Kevin!")
+}else {
+    elHeading.textContent = ("Kevin, ozgina sabr qilish kerak bo'lar ekan.")
+}
